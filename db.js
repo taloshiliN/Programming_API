61c8defe606911a56d832c1ef92bce33bc5cfadc
Taloshili Nghiwewelekwa <nghiwewelekwataloshili@gmail.com>
2024-11-01 01:16:13 +0200

    Version 4

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: `postgres://${process.env.SUPABASE_URL}:${process.env.SUPABASE_KEY}@db.your-supabase-url.supabase.co:5432/postgres`
+    connectionString: `postgres://${process.env.SUPABASE_USER}:${process.env.SUPABASE_PASSWORD}@ztuqcmzavvmfornraavo.supabase.co:5432/postgres`
 });
+
 
 module.exports = pool;
