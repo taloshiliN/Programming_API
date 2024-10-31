eb58958abc289b434f2a08ff81e1ef1606a277a3
Taloshili Nghiwewelekwa <nghiwewelekwataloshili@gmail.com>
2024-11-01 01:32:59 +0200

    Version 5

--- db.js
+++ db.js
@@ -6,5 +6,4 @@ const pool = new Pool({
     connectionString: `postgres://${process.env.SUPABASE_USER}:${process.env.SUPABASE_PASSWORD}@ztuqcmzavvmfornraavo.supabase.co:5432/postgres`
 });
 
-
 module.exports = pool;
